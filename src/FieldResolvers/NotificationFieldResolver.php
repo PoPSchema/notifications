c21f1010ce2effa344a6ad6854f3f6b550efec5f
Leonardo Losoviz <leo@getpop.org>
2020-06-11 16:01:32 +0800

    Added non-nullable to fields

--- src/FieldResolvers/NotificationFieldResolver.php
+++ src/FieldResolvers/NotificationFieldResolver.php
@@ -88,6 +88,31 @@ class NotificationFieldResolver extends AbstractDBDataFieldResolver
         return $types[$fieldName] ?? parent::getSchemaFieldType($typeResolver, $fieldName);
     }
 
+    public function isSchemaFieldResponseNonNullable(TypeResolverInterface $typeResolver, string $fieldName): bool
+    {
+        $nonNullableFieldNames = [
+            'action',
+            'objectType',
+            'objectID',
+            'userID',
+            'histTime',
+            'histTimeNogmt',
+            'histTimeReadable',
+            'status',
+            'isStatusRead',
+            'isStatusNotRead',
+            'isPostNotification',
+            'isUserNotification',
+            'isCommentNotification',
+            'isTaxonomyNotification',
+            'isAction',
+        ];
+        if (in_array($fieldName, $nonNullableFieldNames)) {
+            return true;
+        }
+        return parent::isSchemaFieldResponseNonNullable($typeResolver, $fieldName);
+    }
+
     public function getSchemaFieldDescription(TypeResolverInterface $typeResolver, string $fieldName): ?string
     {
         $translationAPI = TranslationAPIFacade::getInstance();
