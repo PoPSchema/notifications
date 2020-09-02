e73f572748195b3531a7df47d99c3b80ba3f4d01
Leonardo Losoviz <leo@getpop.org>
2020-09-02 12:44:18 +0800

    Added object type to $resultItem in `resolveValue`

--- src/FieldResolvers/NotificationFieldResolver.php
+++ src/FieldResolvers/NotificationFieldResolver.php
@@ -169,8 +169,22 @@ class NotificationFieldResolver extends AbstractDBDataFieldResolver
         return $schemaFieldArgs;
     }
 
-    public function resolveValue(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = [], ?array $variables = null, ?array $expressions = null, array $options = [])
-    {
+    /**
+     * @param array<string, mixed> $fieldArgs
+     * @param array<string, mixed>|null $variables
+     * @param array<string, mixed>|null $expressions
+     * @param array<string, mixed> $options
+     * @return mixed
+     */
+    public function resolveValue(
+        TypeResolverInterface $typeResolver,
+        object $resultItem,
+        string $fieldName,
+        array $fieldArgs = [],
+        ?array $variables = null,
+        ?array $expressions = null,
+        array $options = []
+    ) {
         $notification = $resultItem;
         $cmsengineapi = \PoP\Engine\FunctionAPIFactory::getInstance();
         $cmscommentsapi = \PoPSchema\Comments\FunctionAPIFactory::getInstance();
